50db255b9672a1c3555709bb0d0af249e4f082d7
maynard <33montefalconyoyon@gmail.com>
2023-06-08 18:30:58 +0800

    Implement TaskResource

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -18,19 +18,10 @@ class TaskResource extends JsonResource
             'type' => 'water-districts',
             'id' => (string)$this->id,
             'attributes' => [
-                'name' => $this->name,
-                'cccNumber' => $this->ccc_number,
-                'dateFiled' => $this->date_filed,
-                'dateIssued' => $this->date_issued,
-                'dateFormed' => $this->date_formed,
-                'totalEmployee' => $this->total_employee,
-                'totalConnection' => $this->total_connection
+                'name' => $this->name
             ],
             'relationships' => [
                 'statuses' => new StatusResource($this->status),
-                'province' => new ProvinceResource($this->province),
-                'category' => new CategoryResource($this->category),
-                'area' => new AreaResource($this->area)
             ]
         ];
     }
